fc3a3b2b47eca8925c578d43750a5a346063e981
cakar <chakarr@gmail.com>
2022-04-26 19:05:47 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express()
+const expressLayouts = require("express-ejs-layouts")
+const indexRouter = require('./routes/index')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', ()=> console.log('connected to Mongodb'))
+
+app.set("view engine", "ejs")
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3002)
