0c894740554c50b13844233765a6c0e42ab18256
cakar <chakarr@gmail.com>
2022-04-27 17:52:37 +0300

    create, list, filter authors added

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
+
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+app.use(express.urlencoded({extended:false}));
+
 const mongoose = require('mongoose')
 require('dotenv').config()
 
@@ -18,7 +22,8 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 
 app.listen(process.env.PORT || 3002)
